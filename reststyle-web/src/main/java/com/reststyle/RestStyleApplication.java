4337364e8406b5c00adf3ee86ba7bbe9135265cb
thefei <1415749952@qq.com>
2021-03-24 21:40:58 +0800

    Initial commit

---
+++ reststyle-web/src/main/java/com/reststyle/RestStyleApplication.java
@@ -0,0 +1,23 @@
+package com.reststyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @version 1.0
+ * @author: TheFei
+ * @Date: 2021-03-24
+ * @Time: 18:26
+ */
+@SpringBootApplication
+public class RestStyleApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RestStyleApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ");
+    }
+}
