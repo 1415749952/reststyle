38fd56e80ecdd23b065160418af3645b75cf245d
thefei <1415749952@qq.com>
2021-03-24 21:56:31 +0800

    清空配置文件

--- reststyle-web/src/main/java/com/reststyle/RestStyleApplication.java
+++ reststyle-web/src/main/java/com/reststyle/RestStyleApplication.java
@@ -18,6 +18,5 @@ public class RestStyleApplication
     public static void main(String[] args)
     {
         SpringApplication.run(RestStyleApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ");
     }
 }
