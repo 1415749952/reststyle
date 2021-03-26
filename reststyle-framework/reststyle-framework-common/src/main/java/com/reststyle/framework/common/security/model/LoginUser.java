e08c84c2aad50d9180475739734b9cecf32beed9
TheFei <1415749952@qq.com>
2021-03-26 17:27:29 +0800

    添加参数校验的一些内容

--- reststyle-framework/reststyle-framework-common/src/main/java/com/reststyle/framework/common/security/model/LoginUser.java
+++ reststyle-framework/reststyle-framework-common/src/main/java/com/reststyle/framework/common/security/model/LoginUser.java
@@ -1,7 +1,7 @@
 package com.reststyle.framework.common.security.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.reststyle.framework.common.security.entity.SysUser;
-import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -13,7 +13,6 @@ import java.util.Set;
  *
  * @author TheFei
  */
-@Data
 public class LoginUser implements UserDetails
 {
     private static final long serialVersionUID = 1L;
@@ -63,16 +62,31 @@ public class LoginUser implements UserDetails
      */
     private SysUser user;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities()
+    public String getToken()
     {
-        return null;
+        return token;
     }
 
+    public void setToken(String token)
+    {
+        this.token = token;
+    }
+
+    public LoginUser()
+    {
+    }
+
+    public LoginUser(SysUser user, Set<String> permissions)
+    {
+        this.user = user;
+        this.permissions = permissions;
+    }
+
+    @JsonIgnore
     @Override
     public String getPassword()
     {
-        return null;
+        return user.getPassword();
     }
 
     @Override
@@ -81,27 +95,135 @@ public class LoginUser implements UserDetails
         return user.getUserName();
     }
 
+    /**
+     * 账户是否未过期,过期无法验证
+     */
+    @JsonIgnore
     @Override
     public boolean isAccountNonExpired()
     {
         return true;
     }
 
+    /**
+     * 指定用户是否解锁,锁定的用户无法进行身份验证
+     *
+     * @return
+     */
+    @JsonIgnore
     @Override
     public boolean isAccountNonLocked()
     {
         return true;
     }
 
+    /**
+     * 指示是否已过期的用户的凭据(密码),过期的凭据防止认证
+     *
+     * @return
+     */
+    @JsonIgnore
     @Override
     public boolean isCredentialsNonExpired()
     {
         return true;
     }
 
+    /**
+     * 是否可用 ,禁用的用户不能身份验证
+     *
+     * @return
+     */
+    @JsonIgnore
     @Override
     public boolean isEnabled()
     {
         return true;
+    }
+
+    public Long getLoginTime()
+    {
+        return loginTime;
+    }
+
+    public void setLoginTime(Long loginTime)
+    {
+        this.loginTime = loginTime;
+    }
+
+    public String getIpaddr()
+    {
+        return ipaddr;
+    }
+
+    public void setIpaddr(String ipaddr)
+    {
+        this.ipaddr = ipaddr;
+    }
+
+    public String getLoginLocation()
+    {
+        return loginLocation;
+    }
+
+    public void setLoginLocation(String loginLocation)
+    {
+        this.loginLocation = loginLocation;
+    }
+
+    public String getBrowser()
+    {
+        return browser;
+    }
+
+    public void setBrowser(String browser)
+    {
+        this.browser = browser;
+    }
+
+    public String getOs()
+    {
+        return os;
+    }
+
+    public void setOs(String os)
+    {
+        this.os = os;
+    }
+
+    public Long getExpireTime()
+    {
+        return expireTime;
+    }
+
+    public void setExpireTime(Long expireTime)
+    {
+        this.expireTime = expireTime;
+    }
+
+    public Set<String> getPermissions()
+    {
+        return permissions;
+    }
+
+    public void setPermissions(Set<String> permissions)
+    {
+        this.permissions = permissions;
+    }
+
+    public SysUser getUser()
+    {
+        return user;
+    }
+
+    public void setUser(SysUser user)
+    {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities()
+    {
+        return null;
     }
 }
