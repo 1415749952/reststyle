12bd5974571ca27953fa6a6686bc103c89c2dedf
TheFei <1415749952@qq.com>
2021-04-06 17:50:06 +0800

    提交原因：修改和新增数据库数据自动生成创建人和创建时间

---
+++ reststyle-framework/reststyle-framework-web/src/main/java/com/reststyle/framework/web/config/mybatis_plus/MetaObjectHandler.java
@@ -0,0 +1,55 @@
+package com.reststyle.framework.web.config.mybatis_plus;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.reststyle.framework.common.security.SecurityUtils;
+import com.reststyle.framework.common.security.model.LoginUser;
+import com.reststyle.framework.common.utils.DateUtils;
+import org.apache.ibatis.reflection.MetaObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:新增和更新的基础数据填充，配合MyBatisPlusConfig使用
+ *
+ * @version 1.0
+ * @author: TheFei
+ * @Date: 2021-04-06
+ * @Time: 17:44
+ */
+@Component
+public class MetaHandler implements MetaObjectHandler
+{
+
+    private static final Logger logger = LoggerFactory.getLogger(MetaHandler.class);
+
+    /**
+     * 新增数据执行
+     *
+     * @param metaObject
+     */
+    @Override
+    public void insertFill(MetaObject metaObject)
+    {
+        LoginUser loginUser = SecurityUtils.getLoginUser();
+        this.setFieldValByName("createTime", DateUtils.getNowDate(), metaObject);
+        this.setFieldValByName("createBy", loginUser.getUsername(), metaObject);
+        this.setFieldValByName("updateTime", DateUtils.getNowDate(), metaObject);
+        this.setFieldValByName("updateBy", loginUser.getUsername(), metaObject);
+    }
+
+    /**
+     * 更新数据执行
+     *
+     * @param metaObject
+     */
+    @Override
+    public void updateFill(MetaObject metaObject)
+    {
+        LoginUser loginUser = SecurityUtils.getLoginUser();
+        this.setFieldValByName("updateTime", DateUtils.getNowDate(), metaObject);
+        this.setFieldValByName("updateBy", loginUser.getUsername(), metaObject);
+    }
+
+}
