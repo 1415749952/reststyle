bd93eec9f2ed13373975816688c9bc8be482377a
TheFei <1415749952@qq.com>
2021-07-13 22:48:39 +0800

    提交原因：完成登陆验证，但是token还是写死的，未动态生成token

--- reststyle-framework/reststyle-framework-web/src/main/java/com/reststyle/framework/web/security/handle/UserLoginSuccessHandler.java
+++ reststyle-framework/reststyle-framework-web/src/main/java/com/reststyle/framework/web/security/handle/UserLoginSuccessHandler.java
@@ -1,9 +1,12 @@
 package com.reststyle.framework.web.security.handle;
 
 import com.reststyle.framework.common.constant.Constants;
+import com.reststyle.framework.common.security.entity.SecurityUser;
 import com.reststyle.framework.common.unite_response.ResultUtil;
 import com.reststyle.framework.common.utils.ServletUtils;
 import com.reststyle.framework.common.utils.json.JacksonUtils;
+import com.reststyle.framework.service.manager.AsyncManager;
+import com.reststyle.framework.service.manager.factory.AsyncFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -35,10 +38,13 @@ public class UserLoginSuccessHandler implements AuthenticationSuccessHandler
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
     {
+        SecurityUser securityUser = (SecurityUser)authentication.getPrincipal();
         // 生成令牌
         String token = "token------写死的token";
-        HashMap<String, String> tokenMap = new HashMap<>(1);
+        HashMap<String, String> tokenMap = new HashMap<>(2);
         tokenMap.put(Constants.TOKEN, token);
+        tokenMap.put("username",securityUser.getUsername());
+        AsyncManager.me().execute(AsyncFactory.recordLogininfor(securityUser.getUsername(), Constants.LOGIN_SUCCESS, "登陆成功"));
         ServletUtils.renderString(response, JacksonUtils.object2Json(ResultUtil.success(tokenMap)), HttpStatus.OK);
     }
 }
