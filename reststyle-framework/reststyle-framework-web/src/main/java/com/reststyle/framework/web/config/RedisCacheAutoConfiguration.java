288f1c78b4eb8400e9156114c41f6748d97778f0
TheFei <1415749952@qq.com>
2021-07-19 16:16:21 +0800

    提交原因：登录查询用户，用户角色，用户权限。做了redis缓存

--- reststyle-framework/reststyle-framework-web/src/main/java/com/reststyle/framework/web/config/RedisCacheAutoConfiguration.java
+++ reststyle-framework/reststyle-framework-web/src/main/java/com/reststyle/framework/web/config/RedisCacheAutoConfiguration.java
@@ -4,8 +4,11 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.Cache;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -30,6 +33,7 @@ import java.time.Duration;
  * @Date: 2021-07-15
  * @Time: 16:22
  */
+@Slf4j
 // 开启redis缓存功能
 @EnableCaching
 // 备注这是一个配置类
@@ -100,6 +104,46 @@ public class RedisCacheAutoConfiguration extends CachingConfigurerSupport
                 // 缓存有效期为一天
                 .entryTtl(Duration.ofDays(1));
         return new RedisCacheManager(redisCacheWriter, redisCacheConfiguration);
+    }
+
+    /**
+     * redis数据操作异常处理
+     * 这里的处理：在日志中打印出错误信息，但是放行
+     * 保证redis服务器出现连接等问题的时候不影响程序的正常运行，使得能够出问题时不用缓存
+     *
+     * @return
+     */
+    @Bean
+    @Override
+    public CacheErrorHandler errorHandler()
+    {
+        CacheErrorHandler cacheErrorHandler = new CacheErrorHandler()
+        {
+            @Override
+            public void handleCacheGetError(RuntimeException e, Cache cache, Object key)
+            {
+                log.error("redis异常：key=[{}]", key, e);
+            }
+
+            @Override
+            public void handleCachePutError(RuntimeException e, Cache cache, Object key, Object value)
+            {
+                log.error("redis异常：key=[{}]", key, e);
+            }
+
+            @Override
+            public void handleCacheEvictError(RuntimeException e, Cache cache, Object key)
+            {
+                log.error("redis异常：key=[{}]", key, e);
+            }
+
+            @Override
+            public void handleCacheClearError(RuntimeException e, Cache cache)
+            {
+                log.error("redis异常：", e);
+            }
+        };
+        return cacheErrorHandler;
     }
 
 }
